f87306de119c17c83649f83296bf1fd60d37955c
Peter Yasi <pyasi8192@gmail.com>
2019-02-09 00:37:39 -0500

    Create method for querying builds

--- pybuildkite/builds.py
+++ pybuildkite/builds.py
@@ -1,19 +1,31 @@
 import datetime
+import urllib
 from enum import Enum
 from pybuildkite.client import Client
 
 
 class BuildState(Enum):
-    RUNNING = 1
-    SCHEDULED = 2
-    PASSED = 3
-    FAILED = 4
-    BLOCKED = 5
-    CANCELED = 6
-    CANCELING = 7
-    SKIPPED = 8
-    NOT_RUN = 9
-    FINISHED = 10
+    RUNNING = "running"
+    SCHEDULED = "scheduled"
+    PASSED = "passed"
+    FAILED = "failed"
+    BLOCKED = "blocked"
+    CANCELED = "canceled"
+    CANCELING = "canceling"
+    SKIPPED = "skipped"
+    NOT_RUN = "not_run"
+    FINISHED = "finished"
+
+# TODO needed?
+class BuildQueryParams(Enum):
+    CREATOR = "creator"
+    CREATED_FROM = "created_from"
+    CREATED_TO = "created_to"
+    FINISHED_FROM = "finished_from"
+    STATE = "state"
+    META_DATA = "meta_data"
+    BRANCH = "branch"
+    COMMIT = "commit"
 
 
 class Builds(Client):
@@ -23,7 +35,7 @@ class Builds(Client):
 
         """
         self.client = client
-        self.path = base_url + 'builds/'
+        self.path = base_url + 'builds'
 
     def list_all(self, creator=None, created_from=None, created_to=None, finished_from=None,
                  state=None, meta_data=None, branch=None, commit=None):
@@ -42,6 +54,29 @@ class Builds(Client):
         self.__validate_dates([created_from, created_to, finished_from])
         self.__is_valid_state(state)
 
+
+        query_params = {
+            "creator": creator,
+            "created_from": created_from,
+            "created_to": created_to,
+            "finished_from": finished_from,
+            "state": state,
+            "meta_data": meta_data,
+            "branch": branch,
+            "commit": commit
+        }
+        query_params = self.__clean_query_params(query_params)
+        return self.client.get(self.path, query_params)
+
+    def __clean_query_params(self, query_params):
+        """
+
+        :param query_params:
+        :return:
+        """
+        return {key: value for key, value in query_params.items() if value is not None}
+
+
     @staticmethod
     def __validate_dates(datetimes):
         for date in datetimes:
@@ -51,7 +86,9 @@ class Builds(Client):
 
     @staticmethod
     def __is_valid_state(state):
-        if state not in BuildState.__members__:
+        if state is None:
+            return
+        if not isinstance(state, BuildState):
             raise NotValidBuildState
 
 
--- pybuildkite/client.py
+++ pybuildkite/client.py
@@ -1,4 +1,5 @@
 import requests
+import urllib
 
 
 class Client(object):
@@ -19,9 +20,21 @@ class Client(object):
     def set_client_access_token(self, access_token):
         self.access_token = access_token
 
-    def get(self, url):
-        response = requests.get(url + "?access_token={}".format(self.access_token))
+    def get(self, url, query_params=None):
+
+        url = self.__create_url(url, query_params)
+        response = requests.get(url)
         response.raise_for_status()
 
         return response.text
 
+    def __create_url(self, url, query_params):
+        """
+
+        :param url:
+        :param query_params:
+        :return:
+        """
+        query_params["access_token"] = self.access_token
+        query_params = urllib.parse.urlencode(query_params)
+        return url + "?" + query_params
