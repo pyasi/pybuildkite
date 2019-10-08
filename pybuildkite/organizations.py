3d5306ac2aeb3df8e6e83a8ec17fb0f057ef30ac
Billie Thompson <billie@billiecodes.com>
2019-10-08 20:37:23 +0100

    Added documentation to organizations class
    For issue #5

--- pybuildkite/organizations.py
+++ pybuildkite/organizations.py
@@ -2,16 +2,32 @@ from pybuildkite.client import Client
 
 
 class Organizations(Client):
-
+    """
+    Organization operations for the Buildkite API
+    """
     def __init__(self, client, base_url):
         """
+        Construct the class
 
+        :param client: API Client
+        :param base_url: Base Url
         """
         self.client = client
         self.path = base_url + 'organizations/'
 
     def list_all(self):
+        """
+        Returns a paginated list of the user’s organizations.
+
+        :return: Paginated list of the user’s organizations.
+        """
         return self.client.get(self.path)
 
     def get_org(self, org_name):
+        """
+        Get an organization
+
+        :param org_name: Organisation slug
+        :return: Organisation
+        """
         return self.client.get(self.path + org_name)
