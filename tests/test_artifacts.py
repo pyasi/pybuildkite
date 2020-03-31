6aa17d8a611829d9ca2890988f9858711b012a58
Victor Maryama <victor.maryama@evbox.com>
2020-03-31 22:12:35 +0200

    Inverted list_artifacts_for_build and _for_job functions.

--- tests/test_artifacts.py
+++ tests/test_artifacts.py
@@ -17,23 +17,23 @@ class TestArtifacts:
         """
         return Mock(get=Mock())
 
-    def test_list_artifacts_for_job(self, fake_client):
+    def test_list_artifacts_for_build(self, fake_client):
         """
-        Test List Artifacts for job
+        Test List Artifacts for build
         """
         artifacts = Artifacts(fake_client, "base")
-        artifacts.list_artifacts_for_job("org_slug", "pipe_slug", "build_no")
+        artifacts.list_artifacts_for_build("org_slug", "pipe_slug", "build_no")
         url = (
             "baseorganizations/org_slug/pipelines/pipe_slug/builds/build_no/artifacts/"
         )
         fake_client.get.assert_called_with(url)
 
-    def test_list_artifacts_for_build(self, fake_client):
+    def test_list_artifacts_for_job(self, fake_client):
         """
-        Test list artifacts for build
+        Test list artifacts for job
         """
         artifacts = Artifacts(fake_client, "base")
-        artifacts.list_artifacts_for_build("org_slug", "pipe_slug", "build_no", 123)
+        artifacts.list_artifacts_for_job("org_slug", "pipe_slug", "build_no", 123)
         url = "baseorganizations/org_slug/pipelines/pipe_slug/builds/build_no/jobs/123/artifacts/"
         fake_client.get.assert_called_with(url)
 
