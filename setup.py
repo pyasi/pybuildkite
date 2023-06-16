1164a9b8bec3ecec4646f78be1f723863b4093e1
Peter Yasi <yasipete@amazon.com>
2023-06-16 09:25:55 -0400

    bump to version 1.2.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pybuildkite",
-    version="1.2.2",
+    version="1.2.3",
     url="https://github.com/pyasi/pybuildkite",
     download_url="https://github.com/pyasi/pybuildkite/archive/master.zip",
     author="Peter Yasi",
